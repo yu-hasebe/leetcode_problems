be5a6ab607fea357401b088642e85fd5877150c9
yu-hasebe <yu.has128@gmail.com>
2020-09-30 19:57:00 +0900

    Refactor n15_3sum

--- src/answers/n15_3sum.rs
+++ src/answers/n15_3sum.rs
@@ -11,12 +11,17 @@ impl Solution {
 
         let mut ret: HashSet<Vec<i32>> = HashSet::new();
         for i in 0..nums.len() {
-            for j in i + 1..nums.len() {
-                let want = -nums[i] - nums[j];
-                if let Ok(k) = nums.binary_search(&want) {
-                    if j < k {
-                        ret.insert(vec![nums[i], nums[j], nums[k]]);
-                    }
+            let (mut j, mut k) = (i + 1, nums.len() - 1);
+            while j < k {
+                let sum = nums[i] + nums[j] + nums[k];
+                if sum > 0 {
+                    k -= 1;
+                } else if sum < 0 {
+                    j += 1;
+                } else {
+                    ret.insert(vec![nums[i], nums[j], nums[k]]);
+                    j += 1;
+                    k -= 1;
                 }
             }
         }
@@ -32,7 +37,7 @@ mod tests {
     #[test]
     fn test_three_sum() {
         let nums = vec![-1, 0, 1, 2, -1, -4];
-        let expect = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        let expect = vec![vec![-1, 0, 1], vec![-1, -1, 2]];
         let get = Solution::three_sum(nums);
         assert_eq!(expect, get);
     }
